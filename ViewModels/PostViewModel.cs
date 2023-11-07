3851bf03003e2e2598850cc16ca8a78472e01ed2
viniciuscgama <coelhovinigama@gmail.com>
2023-11-06 22:31:51 -0300

    Add MVVM Community Tool

--- ViewModels/PostViewModel.cs
+++ ViewModels/PostViewModel.cs
@@ -1,26 +1,66 @@
-﻿using MVVM_API_SampleProject.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MVVM_API_SampleProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Diagnostics;
 
 namespace MVVM_API_SampleProject.ViewModels
 {
-    public class PostViewModel
+    internal partial class PostViewModel : ObservableObject, IDisposable
     {
-        public Post Post {  get; set; }
+        HttpClient client;
 
-        public PostViewModel()
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "https://jsonplaceholder.typicode.com";
+
+        [ObservableProperty]
+        public int _UserId;
+
+        [ObservableProperty]
+        public int _Id;
+
+        [ObservableProperty]
+        public string _Title;
+
+        //Uma coleção de Post
+        [ObservableProperty]
+        public ObservableCollection<Post> _posts;
+
+        public  PostViewModel()
         {
-            Post = new Post()
-            { 
-            
-                UserId = 1,
-                Id = 1,
-                Title = "Titulo",
-                Body = "Texto"
-            };
+            client = new HttpClient();
+            Posts = new ObservableCollection<Post>();
+            _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         }
+
+        //Consumir a API Rest -> Criação dos Comandos 
+        public ICommand GetPostsCommand => new Command(async () => await LoadPostsAsync());
+
+        private async Task LoadPostsAsync()
+        {
+            var url = $"{baseUrl}/posts";
+            try
+            {
+                var response = await client.GetAsync(url);
+                if(response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Posts = JsonSerializer.Deserialize<ObservableCollection<Post>>(content, _serializerOptions);
+
+                }
+            }
+            catch (Exception e){ }
+        }
+        public void Dispose ()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
--- ViewModels/ToDoViewModel.cs
+++ ViewModels/ToDoViewModel.cs
@@ -1,25 +1,60 @@
-﻿using MVVM_API_SampleProject.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MVVM_API_SampleProject.Models;
+using System.Collections.ObjectModel;
+using System.Text.Json;
+using System.Windows.Input;
 
 namespace MVVM_API_SampleProject.ViewModels
 {
-    public class ToDoViewModel
+    internal partial class ToDoViewModel : ObservableObject, IDisposable
     {
-        public ToDo ToDo {  get; set; }
+        HttpClient client;
+
+        JsonSerializerOptions _serializerOptions;
+        string baseUrl = "https://jsonplaceholder.typicode.com";
+
+        [ObservableProperty]
+        public int _UserId;
+
+        [ObservableProperty]
+        public int _Id;
+
+        [ObservableProperty]
+        public string _Title;
+
+        //Uma coleção de Post
+        [ObservableProperty]
+        public ObservableCollection<ToDo> _posts;
 
         public ToDoViewModel()
         {
-            ToDo = new ToDo()
-            {
-                UserId = 1,
-                Id = 1,
-                Title = "Lavar a roupa",
-                Completed=false
-            };
+            client = new HttpClient();
+            Posts = new ObservableCollection<ToDo>();
+            _serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         }
+
+        //Consumir a API Rest -> Criação dos Comandos 
+        public ICommand GetPostsCommand => new Command(async () => await LoadPostsAsync());
+
+        private async Task LoadPostsAsync()
+        {
+            var url = $"{baseUrl}/posts";
+            try
+            {
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Posts = JsonSerializer.Deserialize<ObservableCollection<ToDo>>(content, _serializerOptions);
+
+                }
+            }
+            catch (Exception e) { }
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
